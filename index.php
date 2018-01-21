4bcab4ae7cdf80861faf0c11e6121a02007ab96c
anatolevna <33937714+anatolevna@users.noreply.github.com>
2018-01-21 11:12:43 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php include('main.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home13</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+</head>
 <body>
+<header>
+    <a href="login.php">Login</a>
+    <a href="reg.php">Registration</a>
+
+</header>
 
 </body>
 </html>
